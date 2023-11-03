1a4cfca0fe09ba80dfe46531598908e09fc96a00
KhaylaniAM <m3wwhee@gmail.com>
2023-11-03 13:17:18 +0700

    first commit

---
+++ crud/crud/Form1.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.IbNoPenjualan = New System.Windows.Forms.Label()
+        Me.IbTotalHarga = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtHarga = New System.Windows.Forms.TextBox()
+        Me.txtJumlah = New System.Windows.Forms.TextBox()
+        Me.txtCari = New System.Windows.Forms.TextBox()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnBatal = New System.Windows.Forms.Button()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(261, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Aplikasi Penjualan Sederhana"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "No Penjualan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 48)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama Barang"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(7, 76)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Haga Barang"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(38, 107)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Jumlah"
+        '
+        'IbNoPenjualan
+        '
+        Me.IbNoPenjualan.AutoSize = True
+        Me.IbNoPenjualan.Location = New System.Drawing.Point(105, 25)
+        Me.IbNoPenjualan.Name = "IbNoPenjualan"
+        Me.IbNoPenjualan.Size = New System.Drawing.Size(39, 13)
+        Me.IbNoPenjualan.TabIndex = 5
+        Me.IbNoPenjualan.Text = "Label6"
+        '
+        'IbTotalHarga
+        '
+        Me.IbTotalHarga.AutoSize = True
+        Me.IbTotalHarga.Location = New System.Drawing.Point(105, 127)
+        Me.IbTotalHarga.Name = "IbTotalHarga"
+        Me.IbTotalHarga.Size = New System.Drawing.Size(13, 13)
+        Me.IbTotalHarga.TabIndex = 6
+        Me.IbTotalHarga.Text = "0"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.IbNoPenjualan)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtNama)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtJumlah)
+        Me.GroupBox1.Controls.Add(Me.txtHarga)
+        Me.GroupBox1.Controls.Add(Me.IbTotalHarga)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(33, 40)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(241, 160)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Input Data"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.btnSimpan)
+        Me.GroupBox2.Controls.Add(Me.txtCari)
+        Me.GroupBox2.Controls.Add(Me.btnBatal)
+        Me.GroupBox2.Controls.Add(Me.btnEdit)
+        Me.GroupBox2.Controls.Add(Me.btnHapus)
+        Me.GroupBox2.Location = New System.Drawing.Point(309, 40)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(207, 160)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Tools"
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(93, 45)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(100, 20)
+        Me.txtNama.TabIndex = 9
+        '
+        'txtHarga
+        '
+        Me.txtHarga.Location = New System.Drawing.Point(93, 73)
+        Me.txtHarga.Name = "txtHarga"
+        Me.txtHarga.Size = New System.Drawing.Size(100, 20)
+        Me.txtHarga.TabIndex = 10
+        '
+        'txtJumlah
+        '
+        Me.txtJumlah.Location = New System.Drawing.Point(93, 104)
+        Me.txtJumlah.Name = "txtJumlah"
+        Me.txtJumlah.Size = New System.Drawing.Size(100, 20)
+        Me.txtJumlah.TabIndex = 11
+        '
+        'txtCari
+        '
+        Me.txtCari.Location = New System.Drawing.Point(22, 134)
+        Me.txtCari.Name = "txtCari"
+        Me.txtCari.Size = New System.Drawing.Size(161, 20)
+        Me.txtCari.TabIndex = 12
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(108, 37)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.btnEdit.TabIndex = 13
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(6, 90)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnHapus.TabIndex = 14
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btnBatal
+        '
+        Me.btnBatal.Location = New System.Drawing.Point(108, 90)
+        Me.btnBatal.Name = "btnBatal"
+        Me.btnBatal.Size = New System.Drawing.Size(75, 23)
+        Me.btnBatal.TabIndex = 15
+        Me.btnBatal.Text = "Batal"
+        Me.btnBatal.UseVisualStyleBackColor = True
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(33, 222)
+        Me.dgv.Name = "dgv"
+        Me.dgv.Size = New System.Drawing.Size(517, 87)
+        Me.dgv.TabIndex = 16
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 127)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(63, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Total Harga"
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(6, 37)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnSimpan.TabIndex = 17
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(71, 116)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 13)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Cari Data"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(720, 321)
+        Me.Controls.Add(Me.dgv)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents IbNoPenjualan As System.Windows.Forms.Label
+    Friend WithEvents IbTotalHarga As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtNama As System.Windows.Forms.TextBox
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNama.TextChanged
+
+    End Sub
+    Friend WithEvents txtHarga As System.Windows.Forms.TextBox
+    Friend WithEvents txtJumlah As System.Windows.Forms.TextBox
+    Friend WithEvents txtCari As System.Windows.Forms.TextBox
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents btnHapus As System.Windows.Forms.Button
+    Friend WithEvents btnBatal As System.Windows.Forms.Button
+    Friend WithEvents dgv As System.Windows.Forms.DataGridView
+im
+        Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        End Sub
+
+        Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+
+        End Sub
+
+        Private Sub IbNoPenjualan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles IbNoPenjualan.Click
+
+        End Sub
+
+        Private Sub dgv_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgv.CellContentClick
+
+        End Sub
+        Friend WithEvents Label6 As System.Windows.Forms.Label
+        Friend WithEvents Label7 As System.Windows.Forms.Label
+        Friend WithEvents btnSimpan As System.Windows.Forms.Button
+    End Class
---
+++ crud/crud/modulKoneksi.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.OleDb
+Module modulKoneksi
+    Public conn As OleDbConnection
+    Public da As OleDbDataAdapter
+    Public ds As DataSet
+    Public cmd As OleDbCommand
+    Public rd As OleDbDataReader
+    Public dgv As New DataGridView
+
+    Public Sub koneksi()
+        conn = New OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data source=c:\User\hp\Desktop\TUGAS RPLG\Database\dbsale.accdb")
+        conn.Open()
+    End Sub
+End Module
